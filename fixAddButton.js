8c1fe3d8cf8ab0f2fdf44211ef090598402b78fc
IzzyCraze365 <johnaisabella3@gmail.com>
2023-08-02 14:12:03 -0400

    finally fixed the add item button

---
+++ fixAddButton.js
@@ -0,0 +1,13 @@
+//let craftingWordMatrix = ["One", "Two", "Three", "Four", "Five"];
+
+
+//! Buttons
+let word = "Jello";
+let addButton = document.getElementById("addItemButton");
+
+addButton.addEventListener("click", add);
+
+  
+function add() {
+    console.log(word, "sup"); //! TEST
+  }
